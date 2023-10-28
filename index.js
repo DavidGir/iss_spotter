354b60f9371ad06b732b4ac90d9280bf6251f8fa
David Giroux <DavidGiroux149@gmail.com>
2023-10-28 00:16:39 -0400

    implemented cb function to display passTimes

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+const { nextISSTimesForMyLocation } = require('./iss');
 
-  console.log('It worked! Returned IP:' , ip);
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  // We have IP then proceed to getting coordinates:
-  fetchCoordsByIP(ip, (error, coords) => {
-    if (error) {
-      console.log("There is an error", error);
-      return;
-    }
-    console.log("Coordinates", coords);
+//   console.log('It worked! Returned IP:' , ip);
+
+//   // We have IP then proceed to getting coordinates:
+//   fetchCoordsByIP(ip, (error, coords) => {
+//     if (error) {
+//       console.log("There is an error", error);
+//       return;
+//     }
+//     console.log("Coordinates", coords);
       
-    // We have coordinates proceed to get ISS flyover times:
-    fetchISSFlyOverTimes(coords, (error, flyoverTimes) => {
-      if (error) {
-        console.log("There is an error", error);
-        return;
-      }
-      console.log("Flyover Times", flyoverTimes);
-    });
-  });
+//     // We have coordinates proceed to get ISS flyover times:
+//     fetchISSFlyOverTimes(coords, (error, flyoverTimes) => {
+//       if (error) {
+//         console.log("There is an error", error);
+//         return;
+//       }
+//       console.log("Flyover Times", flyoverTimes);
+//     });
+//   });
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  // Loop through passTimes:
+  for (const pass of passTimes) {
+    // Initialize variable to contain a date object with current date and time:
+    const date = new Date();
+    // Contain duration value from the pass object:
+    const duration = pass.duration;
+    // To capture details of each passes:
+    console.log(`Next pass at ${date} for ${duration} seconds!`);
+  }
 });
